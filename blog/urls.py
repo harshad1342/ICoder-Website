c13119f5b0fe859140f4c96b5ad87e54e6b485d5
harshad1342 <kp114565@gmail.com>
2020-08-23 20:39:07 +0530

    Icoder-website

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    # API to post comment
+    path('postComment', views.postComment, name="postComment"),
+
+    path('', views.blogHome, name='blogHome'),
+    path('<str:slug>', views.blogPost, name='blogPost'),
+
+
+]
---
+++ static/js/about.js
@@ -0,0 +1,7 @@
+#abc{
+			width:70px;
+			height:70px;
+			background: black;
+			color: white;
+			font-size: 70px;
+		}
